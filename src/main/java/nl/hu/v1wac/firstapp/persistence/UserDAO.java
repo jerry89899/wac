e17af7fadef2766551ccb3badd73de6da16912df
jerry89899 <jerrylooman@hotmail.com>
2017-06-19 19:12:47 +0200

    het werkt

--- src/main/java/nl/hu/v1wac/firstapp/persistence/UserDAO.java
+++ src/main/java/nl/hu/v1wac/firstapp/persistence/UserDAO.java
@@ -5,25 +5,26 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class UserDao extends BaseDAO {
-	public String findRoleForUsernameAndPassword(String username, String password) {
-		String role = null;
-		String query = "SELECT role FROM useraccount WHERE username = ? AND password = ?";
+public class UserDAO extends BaseDAO {
+	 public String findRoleForUsernameAndPassword(String username, String password) {
+	 String role = null;
+	 String query = "SELECT role FROM useraccount WHERE username = ? AND password = ?";
 
-		try (Connection con = super.getConnection()) {
+	 try (Connection con = super.getConnection()) {
 
-			PreparedStatement pstmt = con.prepareStatement(query);
-			pstmt.setString(1, username);
-			pstmt.setString(2, password);
+	 PreparedStatement pstmt = con.prepareStatement(query);
+	 pstmt.setString(1, username);
+	 pstmt.setString(2, password);
 
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next())
-				role = rs.getString("role");
+	 ResultSet rs = pstmt.executeQuery();
+	 if (rs.next())
+	 role = rs.getString("role");
 
-		} catch (SQLException sqle) {
-			sqle.printStackTrace();
-		}
+	 } catch (SQLException sqle) {
+	 sqle.printStackTrace();
+	 }
 
-		return role;
+	 return role;
+	 }
 	}
-}
+
--- src/main/java/nl/hu/v1wac/firstapp/webservices/AuthenticationResource.java
+++ src/main/java/nl/hu/v1wac/firstapp/webservices/AuthenticationResource.java
@@ -14,34 +14,36 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
-import nl.hu.v1wac.firstapp.persistence.UserDao;
+import nl.hu.v1wac.firstapp.persistence.UserDAO;
 
 @Path("/authentication")
 public class AuthenticationResource {
-	final static public Key key = MacProvider.generateKey();
+	 final static public Key key = MacProvider.generateKey();
+	 @POST
+	 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	 public Response authenticateUser(@FormParam("username") String username,
+	 @FormParam("password") String password) {
+	 try {
+	 // Authenticate the user against the database
+	 UserDAO dao = new UserDAO();
+	 String role = dao.findRoleForUsernameAndPassword(username, password);
 
-	@POST
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	public Response authenticateUser(@FormParam("username") String username, @FormParam("password") String password) {
-		try {
-			// Authenticate the user against the database
-			UserDao dao = new UserDao();
-			String role = dao.findRoleForUsernameAndPassword(username, password);
+	 if (role == null) { throw new IllegalArgumentException("No user found!"); }
 
-			if (role == null) {
-				throw new IllegalArgumentException("No user found!");
-			}
+	 // Issue a token for the user
+	 Calendar expiration = Calendar.getInstance();
+	 expiration.add(Calendar.MINUTE, 30);
 
-			// Issue a token for the user
-			Calendar expiration = Calendar.getInstance();
-			expiration.add(Calendar.MINUTE, 30);
-
-			String token = Jwts.builder().setSubject(username).claim("role", role).setExpiration(expiration.getTime())
-					.signWith(SignatureAlgorithm.HS512, key).compact();
-			// Return the token on the response
-			return Response.ok(token).build();
-		} catch (JwtException | IllegalArgumentException e) {
-			return Response.status(Response.Status.UNAUTHORIZED).build();
-		}
-	}
-}
+	 String token = Jwts.builder()
+	 .setSubject(username)
+	 .claim("role", role)
+	 .setExpiration(expiration.getTime())
+	 .signWith(SignatureAlgorithm.HS512, key)
+	 .compact();
+	 // Return the token on the response
+	 return Response.ok(token).build();
+	 } catch (JwtException | IllegalArgumentException e) {
+	 return Response.status(Response.Status.UNAUTHORIZED).build();
+	 }
+	 }
+	} 
